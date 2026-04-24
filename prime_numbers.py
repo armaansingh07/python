b06ec1cd921f233a04278e3397b64fa1a4302aaf
armaansingh07 <25011a0308@jntuhceh.ac.in>
2026-04-24 17:57:56 +0530

    Create prime_numbers.py

---
+++ prime_numbers.py
@@ -0,0 +1,13 @@
+num = 29
+is_prime = True
+
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            is_prime = False
+            break
+
+if is_prime and num > 1:
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
